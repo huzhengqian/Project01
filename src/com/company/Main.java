ae96fd8e61cc6965f1fe6f11e896d2b5d66aff29
lidongdong1354 <ldd1354@hanweb.com>
2021-07-12 10:44:22 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    private int i;
+
+
+    public static void main(String[] args) {
+        int num1=10;
+
+        ArrayList list=new ArrayList();
+        list.add(123);
+        list.add(345);
+        list.add(567);
+
+
+    }
+}
